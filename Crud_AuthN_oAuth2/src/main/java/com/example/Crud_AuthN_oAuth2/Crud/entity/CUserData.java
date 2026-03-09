c1d8e41b67ad6e954bcabb282dd8d539b20c8e11
ParthCoder89 <parthgupta5041@gmail.com>
2026-03-09 22:08:06 +0530

    Commit the Entity Class of this project

---
+++ Crud_AuthN_oAuth2/src/main/java/com/example/Crud_AuthN_oAuth2/Crud/entity/CUserData.java
@@ -0,0 +1,31 @@
+package com.example.Crud_AuthN_oAuth2.Crud.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table
+
+public class CUserData {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String firstName;
+    @Column
+    private String lastName;
+    @Column(unique = true)
+    private String email;
+    @Column
+    private String password;
+    @Column
+    private String role;
+}
