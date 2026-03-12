9c2d86bf8dc86b1c2b8b0bc6270ae65f64845dcf
ParthCoder89 <parthgupta5041@gmail.com>
2026-03-12 22:37:53 +0530

    Commit the AUserData Class means Entity of this project

---
+++ Crud_AuthN_oAuth2/src/main/java/com/example/Crud_AuthN_oAuth2/Auth/Entity/AUserData.java
@@ -0,0 +1,29 @@
+package com.example.Crud_AuthN_oAuth2.Auth.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table
+@NoArgsConstructor
+@AllArgsConstructor
+public class AUserData {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String firstName;
+    @Column
+    private String lastName;
+    @Column(unique = true)
+    private String email;
+    @Column
+    private String password;
+    @Column
+    private String role;
+}
